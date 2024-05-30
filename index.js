f4e986a832d8de3b00160a188c8c8bd811709fab
tomwuchenhe <123397789+tomwuchenhe@users.noreply.github.com>
2024-05-30 12:47:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 for (var i = 0; i < a.relations.length; i++){
     console.log("relation " + i + " for " + a.relations[i].lastName + " " + a.relations[i].firstName + " are")
     for (var j = 0; j < a.relations[i].history.length; j++) { 
-        if (a.relations[i].history[j].key == 17) {
+        if (a.relations[i].history[j].key == 17 || a.relations[i].history[j].key === "Security") {
             alert("In security check")
         }
         console.log(a.relations[i].history[j].key) 
